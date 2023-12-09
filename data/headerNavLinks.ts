c399ad0d66bb6acfd0180f1530219c59187a9ce1
Marco Besier <marcobesier@hey.com>
2023-12-09 22:17:48 +0100

    Add links to newsletter and academy

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -2,6 +2,8 @@ const headerNavLinks = [
   { href: '/blog', title: 'Blog' },
   // { href: '/tags', title: 'Tags' },
   // { href: '/projects', title: 'Projects' },
+  { href: 'https://3blocks.xyz', title: 'Newsletter' },
+  { href: 'https://membership.marcobesier.com', title: 'Academy' },
   { href: '/about', title: 'About' },
 ]
 
